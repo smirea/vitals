d2d743d4b7cfd25c2547ff43b2f606a9233b883d
stefan <me@stefanmirea.com>
2026-03-26 09:23:16 -0400

    updated models

--- server/src/utils/models.ts
+++ server/src/utils/models.ts
@@ -4,8 +4,8 @@ import env from 'server/env';
 const openrouter = createOpenRouter({ apiKey: env.OPENROUTER_API_KEY });
 
 const models = {
-	smart_and_expensive: openrouter('google/gemini-3.1-pro,google/gemini-3.1-pro-preview'),
-	document_parser_cheap: openrouter('google/gemini-3-flash,google/gemini-3-flash-preview'),
+	smart_and_expensive: openrouter('google/gemini-3.1-pro-preview'),
+	document_parser_cheap: openrouter('google/gemini-3-flash-preview'),
 } as const;
 
 export default models;
